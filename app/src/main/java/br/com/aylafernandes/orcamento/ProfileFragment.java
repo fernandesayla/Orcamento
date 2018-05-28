c36649a4a6a4904354c13115dfeaedebc8ff9fa4
aylafernandes <fernandes.ayla@gmail.com>
2018-05-28 03:11:23 -0300

    Fragment e Recycler

--- app/src/main/java/br/com/aylafernandes/orcamento/ProfileFragment.java
+++ app/src/main/java/br/com/aylafernandes/orcamento/ProfileFragment.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,6 +21,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class ProfileFragment extends Fragment {
 
 
+
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -27,34 +30,44 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
         // Inflate the layout for this fragment
-        getUser();
-
-
-
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-
-    }
-
-
-
-    private void  getUser(){
+        TextView mMameAndSurname = view.findViewById(R.id.nameAndSurname);
+        ImageView imageView = view.findViewById(R.id.profileImage);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
-            // Name, email address, and profile photo Url
+            imageView.setImageURI(user.getPhotoUrl());
+            mMameAndSurname.setText(user.getDisplayName());
 
-
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getToken() instead.
-            String uid = user.getUid();
-
-            Log.d("USER:", email + " - "+  photoUrl + " - "+  name);
         }
+
+
+        return view;
+
     }
+
+//
+//    private void  getUser(){
+//
+//        if (user != null) {
+//            // Name, email address, and profile photo Url
+//
+//
+//            String name = user.getDisplayName();
+//
+//            String email = user.getEmail();
+//            Uri photoUrl = user.getPhotoUrl();
+//
+//
+//
+//            // The user's ID, unique to the Firebase project. Do NOT use this value to
+//            // authenticate with your backend server, if you have one. Use
+//            // FirebaseUser.getToken() instead.
+//            String uid = user.getUid();
+//
+//            updateUI(user);
+//           // Log.d("USER:", email + " - "+  photoUrl + " - "+  name);
+//        }
+//    }
 
 }
