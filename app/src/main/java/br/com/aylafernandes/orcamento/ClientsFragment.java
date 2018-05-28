6d9626a8d09ae02f5c92e5577261888da9540672
aylafernandes <fernandes.ayla@gmail.com>
2018-05-28 04:14:27 -0300

    Fragment e Recycler

--- app/src/main/java/br/com/aylafernandes/orcamento/ClientsFragment.java
+++ app/src/main/java/br/com/aylafernandes/orcamento/ClientsFragment.java
@@ -9,14 +9,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import br.com.aylafernandes.orcamento.adapter.ClientAdapter;
+import br.com.aylafernandes.orcamento.adapter.ClientsAdapter;
+import br.com.aylafernandes.orcamento.models.Clients;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ClientsFragment extends Fragment {
-
+    List<Clients> listClients = Collections.emptyList();
+    private DatabaseReference mDatabase;
 
     public ClientsFragment() {
         // Required empty public constructor
@@ -32,9 +42,15 @@ public class ClientsFragment extends Fragment {
             // Inflate the layout for this fragment
             View rootView = inflater.inflate(R.layout.fragment_clients, container, false);
 
+
             RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.rv_fragment_clientes);
+
+
             rv.setHasFixedSize(true);
-            ClientAdapter adapter = new ClientAdapter(new String[]{"Example One", "Example Two", "Example Three", "Example Four", "Example Five" , "Example Six" , "Example Seven"});
+            ClientsAdapter adapter = new ClientsAdapter(getContext(), criaBase());
+
+
+           // ClientAdapter adapter = new ClientAdapter(new String[]{"Example One", "Example Two", "Example Three", "Example Four", "Example Five" , "Example Six" , "Example Seven"});
             rv.setAdapter(adapter);
 
             LinearLayoutManager llm = new LinearLayoutManager(getActivity());
@@ -43,6 +59,18 @@ public class ClientsFragment extends Fragment {
             return rootView;
 
 
+    }
+
+    private List<Clients> criaBase(){
+
+        Clients provaPortugues = new Clients("Portugues", "25/05/2016" );
+
+
+        Clients provaMatematica = new Clients("Matematica", "27/05/2016");
+
+        List<Clients> clients = Arrays.asList(provaPortugues, provaMatematica);
+
+        return clients;
     }
 
 }
